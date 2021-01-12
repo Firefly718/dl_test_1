6a0e37ef5ae7a35c9a91697fbc9ec3632392b1e3
Firefly718 <System.BLA.16@yandex.ru>
2021-01-12 16:33:26 +0200

    added slider

--- default-js/script.js
+++ default-js/script.js
@@ -1,8 +1,29 @@
+//  percent hover animation
 $( document ).ready(function(){
   $( ".header__discount" ).mouseenter(function(){
     $( ".header__percent" ).css( "transform", "rotate(360deg)" );
   });
   $( ".header__discount" ).mouseleave(function(){
     $( ".header__percent" ).css( "transform", "rotate(0deg)" ); 
+  });
+});
+
+
+
+// slick slider
+$(document).ready(function(){
+  $('.slider').slick({
+    dots: true,
+    arrows: true,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    // fade: true,
+    // cssEase: 'linear',
+    infinite: true,
+    lazyLoad: 'ondemand',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: '<div class="arrow arrow__right"></div>',
+    prevArrow: '<div class="arrow arrow__left"></div>'
   });
 });
